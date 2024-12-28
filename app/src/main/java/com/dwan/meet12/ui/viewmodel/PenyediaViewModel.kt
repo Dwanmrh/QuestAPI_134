a7fbce8f19357a441235752c2dc8bef120f8ed1d
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-29 01:36:45 +0700

    Membuat PenyediaViewModel

--- app/src/main/java/com/dwan/meet12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/dwan/meet12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,7 +1,17 @@
 package com.dwan.meet12.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.dwan.meet12.MahasiswaApplications
 
 object PenyediaViewModel{
-    val Factory = viewModelFactory{}
+    val Factory = viewModelFactory{
+        initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        initializer { InsertViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+    }
+
+    fun CreationExtras.MahasiswaApplications(): MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
 }
