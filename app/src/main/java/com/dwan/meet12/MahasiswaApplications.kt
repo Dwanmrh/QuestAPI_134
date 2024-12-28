c3f28f5e294dff1fa8b5918661cde9825cf92534
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-28 21:32:26 +0700

    Memperbaiki pembuatan kelas MahasiswaApplications

--- app/src/main/java/com/dwan/meet12/MahasiswaApplications.kt
+++ app/src/main/java/com/dwan/meet12/MahasiswaApplications.kt
@@ -1,8 +1,10 @@
-package com.dwan.meet12.repository
+package com.dwan.meet12
 
 import android.app.Application
+import com.dwan.meet12.repository.AppContainer
+import com.dwan.meet12.repository.MahasiswaContainer
 
-class MahasiswaApplications: Application() {
+class MahasiswaApplications:Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
