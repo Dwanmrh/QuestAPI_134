539addc05506f2f39b5d0086a3ae4790c47c0446
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-28 21:57:18 +0700

    Membuat InsertViewModel untuk mengelola state form insert mahasiswa

---
+++ app/src/main/java/com/dwan/meet12/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,33 @@
+package com.dwan.meet12.ui.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dwan.meet12.model.Mahasiswa
+import com.dwan.meet12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+// ViewModel untuk mengatur data dan logika form tambah mahasiswa
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+
+    // Data untuk menyimpan keadaan form (seperti input dari pengguna)
+    var uiState by mutableStateOf(InsertUiState())
+    private set
+
+    // Fungsi untuk mengubah data form ketika ada input dari pengguna
+    fun updateInsertMhsState(insertUiEvent.InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent) // Perbarui data berdasarkan event
+    }
+
+    // Fungsi untuk menambahkan data mahasiswa ke database
+    suspend fun insertMhs() {
+        viewModelScope.launch { // Menjalankan proses di latar belakang (tidak mengganggu UI)
+            try {
+                // Mengambil data dari form dan mengirimnya ke repository
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception) {
+                e.printStackTrace() // Menangani error jika terjadi masalah
+            }
+        }
+    }
+}
