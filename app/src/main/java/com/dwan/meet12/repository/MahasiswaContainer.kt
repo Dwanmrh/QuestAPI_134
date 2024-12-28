e5d7be41e55f85195a140d924526bf617232e955
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-28 21:11:21 +0700

    Memperbarui baseurl mahasiswa container

--- app/src/main/java/com/dwan/meet12/repository/MahasiswaContainer.kt
+++ app/src/main/java/com/dwan/meet12/repository/MahasiswaContainer.kt
@@ -11,7 +11,7 @@ interface AppContainer {
 }
 
 class MahasiswaContainer : AppContainer {
-    private val baseurl = "http:// /umyTI/"
+    private val baseurl = "http://10.0.2.2:8000/umyTI/" // localhost diganti ip kalo run di hp
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
