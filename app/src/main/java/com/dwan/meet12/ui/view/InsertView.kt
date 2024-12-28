10a949b9e3d46dd1162a6bbd43d6c23c4b2790d0
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-28 23:54:40 +0700

    Membuat fungsi dasar untuk membuat tampilan interaktif

--- app/src/main/java/com/dwan/meet12/ui/view/InsertView.kt
+++ app/src/main/java/com/dwan/meet12/ui/view/InsertView.kt
@@ -1,6 +1,13 @@
 package com.dwan.meet12.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dwan.meet12.ui.navigation.DestinasiNavigasi
+import com.dwan.meet12.ui.viewmodel.InsertViewModel
+import com.dwan.meet12.ui.viewmodel.PenyediaViewModel
 
 // Membuat objek `DestinasiEntry` yang mengimplementasikan interface `DestinasiNavigasi`
 object DestinasiEntry : DestinasiNavigasi {
@@ -10,4 +17,15 @@ object DestinasiEntry : DestinasiNavigasi {
 
     // Mendefinisikan properti `titleRes` yang mengacu pada string "Entry Mhs"
     override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    // Membuat coroutine scope untuk operasi asynchronous di dalam composable ini
+    val coroutineScope = rememberCoroutineScope()
 }
