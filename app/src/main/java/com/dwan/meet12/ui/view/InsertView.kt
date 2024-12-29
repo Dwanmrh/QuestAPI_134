d47dbe931d2c00b9bf801271ebec583135469292
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-30 01:51:53 +0700

    Melengkapi code yang kurang

--- app/src/main/java/com/dwan/meet12/ui/view/InsertView.kt
+++ app/src/main/java/com/dwan/meet12/ui/view/InsertView.kt
@@ -2,23 +2,38 @@ package com.dwan.meet12.ui.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dwan.meet12.ui.customwidget.CustomTopAppBar
 import com.dwan.meet12.ui.navigation.DestinasiNavigasi
 import com.dwan.meet12.ui.viewmodel.InsertUiEvent
+import com.dwan.meet12.ui.viewmodel.InsertUiState
 import com.dwan.meet12.ui.viewmodel.InsertViewModel
 import com.dwan.meet12.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 // Membuat objek `DestinasiEntry` yang mengimplementasikan interface `DestinasiNavigasi`
 object DestinasiEntry : DestinasiNavigasi {
@@ -27,18 +42,73 @@ object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
 
     // Mendefinisikan properti `titleRes` yang mengacu pada string "Entry Mhs"
-    override val titleRes = "Entry Mhs"
+    override val titleRes = "Insert Mhs"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntryMhsScreen(
     navigateBack: () -> Unit,
+    nim: String? = null, // Tambahkan parameter nim dengan nilai default null
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     // Membuat coroutine scope untuk operasi asynchronous di dalam composable ini
     val coroutineScope = rememberCoroutineScope()
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.insertUiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,6 +119,9 @@ fun FormInput(
     onValueChange: (InsertUiEvent) -> Unit = {}, // Lambda untuk menangani perubahan nilai input
     enabled: Boolean = true // Parameter untuk mengontrol apakah input dapat diubah atau tidak (default: true)
 ) {
+    val jenisKelamin = listOf("Laki-laki", "Perempuan")
+    val kelas = listOf("A", "B", "C", "D",)
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
@@ -72,14 +145,25 @@ fun FormInput(
             singleLine = true
         )
         // Input teks untuk "Jenis Kelamin"
-        OutlinedTextField(
-            value = insertUiEvent.jenisKelamin,
-            onValueChange = { onValueChange(insertUiEvent.copy(jenisKelamin = it)) },
-            label = { Text("Jenis Kelamin") },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
-        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis Kelamin")
+        Row(modifier = Modifier.fillMaxWidth())
+        {
+            jenisKelamin.forEach{ jk ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = insertUiEvent.jenisKelamin == jk,
+                        onClick = {
+                            onValueChange(insertUiEvent.copy(jenisKelamin = jk))
+                        },
+                    )
+                    Text(text = jk,)
+                }
+            }
+        }
         // Input teks untuk "Alamat"
         OutlinedTextField(
             value = insertUiEvent.alamat,
@@ -90,14 +174,26 @@ fun FormInput(
             singleLine = true
         )
         // Input teks untuk "Kelas"
-        OutlinedTextField(
-            value = insertUiEvent.kelas,
-            onValueChange = { onValueChange(insertUiEvent.copy(kelas = it)) },
-            label = { Text("Kelas") },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
-        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Kelas")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            kelas.forEach{ kelas ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = insertUiEvent.kelas == kelas,
+                        onClick = {
+                            onValueChange(insertUiEvent.copy(kelas = kelas))
+                        },
+                    )
+                    Text(text = kelas,)
+                }
+            }
+        }
         // Input teks untuk "Angkatan"
         OutlinedTextField(
             value = insertUiEvent.angkatan,
