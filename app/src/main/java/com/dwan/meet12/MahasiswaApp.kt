c5d3f93c66fbee7b4ada388dd2872e01f190927e
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-29 22:37:13 +0700

    Membuat ulang MahasiswaApp

---
+++ app/src/main/java/com/dwan/meet12/MahasiswaApp.kt
@@ -0,0 +1,36 @@
+package com.dwan.meet12
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.dwan.meet12.ui.navigation.PengelolaHalaman
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MahasiswaApp(modifier: Modifier = Modifier) {
+
+    // Mengatur perilaku scroll untuk TopAppBar
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+
+        // Menambahkan scroll behavior ke Scaffold
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        // topBar = { TopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        // Area konten utama
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            // Menampilkan halaman dengan navigasi
+            PengelolaHalaman()
+        }
+    }
+}
